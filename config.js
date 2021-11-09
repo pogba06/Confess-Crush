c4c0725b233f5f02783d9619d1a94d731c9a5fdc
pogba06 <93985843+pogba06@users.noreply.github.com>
2021-11-09 20:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "love",
+    introTitle: 'nguyễn thị thanh trà',
+    introDesc: `nàng không phải trang sức giúp ta thêm lấp lánh,
+    nàng đơn giản là một góc bình yên nhỉ để bỏ vào tim
+    và không phải xuân diệu nên ta chẳng vội vàng
+    tình ta đơn điệu vẫn nghìn năm đợi nàng`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'tình yêu của anh là mãi mãi, saig gòn hà nội anh trãi dài 🥰',
+    desc: 'khoảng cách địa lý dù có bao xa , chảo dầu núi lữa anh cũng băng qua ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
